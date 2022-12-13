fe7b3c29f68914611a3a4857dca1d7f7f85216ce
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-13 15:23:58 +0100

    fixed import path

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -15,7 +15,7 @@ import {
 } from '@shopify/hydrogen';
 import {HeaderFallback, EventsListener} from '~/components';
 import {NotFound} from '~/components/index.server';
-import NostoComponent from '~/NostoComponents.client';
+import NostoComponent from '~/components/NostoComponents.client';
 
 function App({request}) {
   const pathname = new URL(request.normalizedUrl).pathname;
